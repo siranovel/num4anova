8e9ce2a9f35376c29c56ff9097b67df82450a0b8
siranovel <siranovel@gmail.com>
2024-04-05 11:49:20 +0900

    Add files via upload

--- sample/myintervalmatcher.rb
+++ sample/myintervalmatcher.rb
@@ -21,8 +21,36 @@ module MyIntervalMatcher
       "#{@expected} expected but got #{@actual}"
     end
   end
+  class Matcher2
+    def initialize(expected, n)
+      @expected = expected
+      @n = n
+    end
+    def matches?(actual)
+        ret = true
+        @actual = actual
+        sz = actual.size
+        sz.times do |i|
+            act_min = actual[i][:min]
+            act_max = actual[i][:max]
+            if (act_min.round(@n) != @expected[i][:min]) then
+                ret = false
+            end
+            if (act_max.round(@n) != @expected[i][:max]) then
+                ret = false
+            end
+        end
+        return ret
+    end
+    def failure_message
+      "#{@expected} expected but got #{@actual}"
+    end
+  end
   def interval(expected, n)
     Matcher.new(expected, n)
+  end
+  def intervals(expected, n)
+    Matcher2.new(expected, n)
   end
 end
 
